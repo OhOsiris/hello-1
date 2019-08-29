f22e2afd74d0c9e1567cd6f34499f65290d0a6fe
VCJH9090 <54286449+VCJH9090@users.noreply.github.com>
2019-08-29 12:58:53 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Chong Jun Hong Vince
+ * Date: 2019/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+ #include<stdlib.h>
+ #include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello Chong Jun Hong Vince\n");
+  printf("My Major is Mechanical Engineering");
+
+  return 0;
+}
